8372a9caa3b745b92406695ba8ba53825b56e122
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-17 10:34:03 -0700

    Created for loop and opened file

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,15 @@
-#put your code here 
+import sys
+from collections import Counter
+
+def separated(file):
+
+    """Returns a dictionary with a count of how many times each word duplicates in file """
+    poem = open(file)
+    print(sys.argv)
+
+    word_count = {}
+
+    for line in poem: 
+        line = line.strip()
+        words = line.split(' ')
+
